9eca0436d71cc5f47e859416790eb66c6eb26767
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-02 19:25:28 +0700

    menambahkan HomeViewModel untuk load data siswa dan kelola state UI

--- app/src/main/java/com/example/prak9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/HomeViewModel.kt
@@ -1,9 +1,40 @@
 package com.example.prak9.viewmodel
 
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.prak9.modeldata.Siswa
+import com.example.prak9.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoriSiswa: RepositorySiswa) : ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositoriSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
 }
