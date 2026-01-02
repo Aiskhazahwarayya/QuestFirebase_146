1d4350e9cc97336d30e6b1eeae6b55e734d1cc3c
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-02 19:21:33 +0700

    menambahkan PenyediaViewModel untuk inisialisasi HomeViewModel dan EntryViewModel

--- app/src/main/java/com/example/prak9/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/PenyediaViewModel.kt
@@ -2,9 +2,17 @@ package com.example.prak9.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak9.repositori.AplikasiDataSiswa
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
         )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+    }
+}
