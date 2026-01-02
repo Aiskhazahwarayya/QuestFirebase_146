2c55cc2f329b12185d3f9fcfb377f955a6c59699
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-02 21:55:08 +0700

    Menambahkan MainActivity dengan integrasi DataSiswaApp

--- app/src/main/java/com/example/prak9/MainActivity.kt
+++ app/src/main/java/com/example/prak9/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prak9.ui.theme.Prak9Theme
+import com.example.prak9.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Prak9Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
