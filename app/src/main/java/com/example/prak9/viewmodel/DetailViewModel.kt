64479e16382a57e2647631a95a064cd6e4d8ea17
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-08 19:48:54 +0700

    menambahkan pengambilan idSiswa dari SavedStateHandle dan status UI detail

--- app/src/main/java/com/example/prak9/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/DetailViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.prak9.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.prak9.modeldata.Siswa
 import com.example.prak9.repositori.RepositorySiswa
+import com.example.prak9.view.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
@@ -13,5 +17,14 @@ sealed interface StatusUIDetail {
 
 class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa)
     : ViewModel() {
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
 
 }
