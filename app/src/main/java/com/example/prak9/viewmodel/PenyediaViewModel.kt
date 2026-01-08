d9e5fefbc04f13ae3d4587848ec1e402babeb59a
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-08 20:00:31 +0700

    menambahkan initializer ViewModel untuk Detail dan Edit Siswa

--- app/src/main/java/com/example/prak9/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.prak9.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak9.repositori.AplikasiDataSiswa
+
+
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
@@ -14,5 +17,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoriSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoriSiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
