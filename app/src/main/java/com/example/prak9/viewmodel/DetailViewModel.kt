606fa163edbc5336389c554f04382ea51762ce78
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-08 19:45:55 +0700

    menambahkan DetailViewModel untuk halaman detail siswa

--- app/src/main/java/com/example/prak9/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/DetailViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.prak9.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.prak9.modeldata.Siswa
+import com.example.prak9.repositori.RepositorySiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa)
+    : ViewModel() {
+
 }
