04f7522e7c3f0d0a761614a8d4ee8d8b2aab27e2
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-01-08 19:30:58 +0700

    menambahkan fungsi validasiInput untuk mengecek kelengkapan data siswa

--- app/src/main/java/com/example/prak9/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/EditViewModel.kt
@@ -34,4 +34,10 @@ RepositorySiswa
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
                 (detailSiswa))
     }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 }
